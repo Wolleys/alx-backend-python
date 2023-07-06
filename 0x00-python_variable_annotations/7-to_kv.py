0f1b79c8c90523e801bbeea56c735118b9e3a19d
Wolleys <wolleysmigaya@gmail.com>
2023-07-06 08:43:18 +0000

    A type-annotated function to_kv that returns a tuple

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""A type-annotated function to_kv that returns a tuple"""
+
+
+import typing
+
+
+def to_kv(k: str, v: typing.Union[int, float]) -> typing.Tuple[str, float]:
+    """returns a tuple of the string & square of v as float"""
+    return (k, float(v * v))
