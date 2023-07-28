894638bd0f63f197ea6e2cff85389ebb15fc5d7f
Wolleys <wolleysmigaya@gmail.com>
2023-07-28 13:11:47 +0000

    The TestGetJson(unittest.TestCase) class that tests the get_json function

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,7 +4,8 @@
 
 import unittest
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import access_nested_map, get_json
+from unittest.mock import patch, Mock
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -37,3 +38,24 @@ class TestAccessNestedMap(unittest.TestCase):
         """
         with self.assertRaises(expected_output) as context:
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """_summary_
+    Args:
+    unittest (_type_): _description_
+    """
+    @parameterized.expand(
+            [
+                ('http://example.com', {'payload': True}),
+                ('http://holberton.io', {'payload': False})
+                ]
+            )
+    def test_get_json(self, url, expected_output):
+        """_summary_
+        """
+        mock_response = Mock()
+        mock_response.json.return_value = expected_output
+        with patch('requests.get', return_value=mock_response):
+            response = get_json(url)
+            self.assertEqual(response, expected_output)
